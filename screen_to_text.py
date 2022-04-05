b816ffdd2916032fc9952d725e1b35cb354d5aa1
Farbkecks <98021172+Farbkecks@users.noreply.github.com>
2022-04-05 19:56:16 +0200

    first commit

---
+++ screen_to_text.py
@@ -0,0 +1,77 @@
+import sys
+from PyQt5 import QtWidgets, QtCore, QtGui
+import tkinter as tk
+from PIL import ImageGrab
+import numpy as np
+import cv2
+from PIL import Image
+from pytesseract import *
+import clipboard
+
+
+#pip install PyQt5
+#pip install numpy
+#pip install opencv-python
+#pip install clipboard
+#pip install pytesseract
+
+class MyWidget(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        root = tk.Tk()
+        screen_width = root.winfo_screenwidth()
+        screen_height = root.winfo_screenheight()
+        self.setGeometry(0, 0, screen_width, screen_height)
+        self.setWindowTitle(' ')
+        self.begin = QtCore.QPoint()
+        self.end = QtCore.QPoint()
+        self.setWindowOpacity(0.3)
+        QtWidgets.QApplication.setOverrideCursor(
+            QtGui.QCursor(QtCore.Qt.CrossCursor)
+        )
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+        # print('Capture the screen...')
+        self.show()
+
+    def paintEvent(self, event):
+        qp = QtGui.QPainter(self)
+        qp.setPen(QtGui.QPen(QtGui.QColor('black'), 3))
+        qp.setBrush(QtGui.QColor(128, 128, 255, 128))
+        qp.drawRect(QtCore.QRect(self.begin, self.end))
+
+    def mousePressEvent(self, event):
+        self.begin = event.pos()
+        self.end = self.begin
+        self.update()
+
+    def mouseMoveEvent(self, event):
+        self.end = event.pos()
+        self.update()
+
+    def mouseReleaseEvent(self, event):
+        self.close()
+
+        x1 = min(self.begin.x(), self.end.x())
+        y1 = min(self.begin.y(), self.end.y())
+        x2 = max(self.begin.x(), self.end.x())
+        y2 = max(self.begin.y(), self.end.y())
+
+        # print(x1, y1, x2, y2)
+        img = ImageGrab.grab(bbox=(x1, y1, x2, y2))
+        clipboard.copy(pytesseract.image_to_string(img))
+
+        # img.save('capture.png')
+        # img = cv2.cvtColor(np.array(img), cv2.COLOR_BGR2RGB)
+
+        # cv2.imshow('Captured Image', img)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    pytesseract.tesseract_cmd = r'C:\Users\fabia\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyWidget()
+    window.show()
+    app.aboutToQuit.connect(app.deleteLater)
+    sys.exit(app.exec_())
